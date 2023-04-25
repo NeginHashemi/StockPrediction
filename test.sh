2b56e7640a1006ce790ec1e3c95c1ce81873339d
NeginHashemi <n.hashemi202d@gmail.com>
2023-04-26 02:55:12 +0430

    Debug 5

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 python3 train_rl.py --stock_filepaths 'data/1_4371167945.csv' \
     --obs_columns 'open' 'close' 'time' \
-    --frames 100000 \ 
-    --flag 'Debug'
+    --frames 100000 --flag 'Debug'
